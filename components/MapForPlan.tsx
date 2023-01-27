21aeaf482f8d88595b5dd5fc6562da0b0f08f1cf
Avery Lin <60205611+avery710@users.noreply.github.com>
2023-01-28 02:44:26 +0800

    feat: Add Multi Tile Layers to map

--- components/MapForPlan.tsx
+++ components/MapForPlan.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMap, FeatureGroup } from 'react-leaflet'
+import { use, useEffect, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMap, FeatureGroup, LayersControl } from 'react-leaflet'
 import { EditControl } from "react-leaflet-draw"
 import { LatLngExpression } from "leaflet"
-import L from "leaflet";
+import L from "leaflet"
 
 export default function MapForPlan() {
     return (
@@ -12,11 +12,60 @@ export default function MapForPlan() {
             scrollWheelZoom={true} 
             style={{ height: "100%", width: "100%" }}
         >
+            <LayersControl position='bottomright'>
+
+                <LayersControl.BaseLayer checked name='開放街圖 OpenStreetMap'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    />
+                </LayersControl.BaseLayer>
+
+                <LayersControl.BaseLayer name='魯地圖 Taiwan Topo'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://rudy.basecamp.tw/taiwan_topo.html">Rudy Taiwan TOPO</a> | &copy; <a href="https://twmap.happyman.idv.tw/map/">地圖產生器</a>'
+                        url='https://tile.happyman.idv.tw/mp/wmts/rudy/gm_grid/{z}/{x}/{y}.png'
+                    />
+                </LayersControl.BaseLayer>
+
+                <LayersControl.BaseLayer name='2020-正射影像圖'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://maps.nlsc.gov.tw">內政部國土測繪中心</a>'
+                        url='https://wmts.nlsc.gov.tw/wmts/PHOTO2020/default/GoogleMapsCompatible/{z}/{y}/{x}.jpeg'
+                    />
+                </LayersControl.BaseLayer>
+
+                <LayersControl.BaseLayer name='2003-臺灣經建3版地形圖'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://gissrv4.sinica.edu.tw/gis/twhgis/">中央研究院臺灣百年歷史地圖</a>'
+                        url='https://gis.sinica.edu.tw/tileserver/file-exists.php?img=TM50K_2003-png-{z}-{x}-{y}'
+                    />
+                </LayersControl.BaseLayer>
+
+                <LayersControl.BaseLayer name='1921-日治臺灣堡圖'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://gissrv4.sinica.edu.tw/gis/twhgis/">中央研究院臺灣百年歷史地圖</a>'
+                        url='https://gis.sinica.edu.tw/tileserver/file-exists.php?img=JM20K_1921-jpg-{z}-{x}-{y}'
+                    />
+                </LayersControl.BaseLayer>
+
+                <LayersControl.Overlay name='便利商店'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://maps.nlsc.gov.tw">內政部國土測繪中心</a>'
+                        url='https://wmts.nlsc.gov.tw/wmts/ConvenienceStore/default/GoogleMapsCompatible/{z}/{y}/{x}.png'
+                    />
+                </LayersControl.Overlay>
+
+                <LayersControl.Overlay name='等高線圖'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://maps.nlsc.gov.tw">內政部國土測繪中心</a>'
+                        url='https://wmts.nlsc.gov.tw/wmts/MOI_CONTOUR_2/default/GoogleMapsCompatible/{z}/{y}/{x}.png'
+                    />
+                </LayersControl.Overlay>
+
+            </LayersControl>
+            
             <DrawingToolBar />
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
             <LocationMarker />
         </MapContainer>
     )
@@ -26,13 +75,6 @@ function LocationMarker(){
     const [position, setPosition] = useState<LatLngExpression | null>(null)
 
     const map = useMap()
-
-    // const currentDot = new L.Icon({
-    //     iconUrl: './location.png',
-    //     iconSize: new L.Point(38, 40),
-    //     iconAnchor: [13, 41],
-    //     popupAnchor:  [-3, -76]
-    // })
 
     useEffect(() => {
         map.locate().on("locationfound", e => {
