a6116cc96fd0bbf44e25fc5bad3fe8d6c51468e9
Avery Lin <60205611+avery710@users.noreply.github.com>
2023-02-10 00:26:58 +0800

    chore: Declare Types in Specific file

---
+++ types.ts
@@ -0,0 +1,8 @@
+/* ----------------- lib ----------------- */
+
+export interface userType {
+    uid: string | null,
+    photoUrl: string | null,
+    email: string | null,
+    username: string | null,
+}
