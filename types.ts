4acf3684512ad0d56f3410fc5c734e493d5b27cb
Avery Lin <60205611+avery710@users.noreply.github.com>
2023-02-12 21:23:28 +0800

    feat: Add GeoPoint type

--- types.ts
+++ types.ts
@@ -1,8 +1,12 @@
-/* ----------------- lib ----------------- */
-
 export interface userType {
     uid: string | null,
     photoUrl: string | null,
     email: string | null,
     username: string | null,
+}
+
+export interface geoPointType {
+    lat: number | null,
+    lng: number | null,
+    descript: string | null,
 }
